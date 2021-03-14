2cd51249a3c5942cd2e93b92925ce1c1c2804d18
Maximilian Tabet <maxtabet@gmail.com>
2021-03-14 17:05:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const name = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'surprise';
+
+function writeCards(name, event) {
+    let newArr = [];
+    for (let i = 0; i < name.length; i++) {
+        newArr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr;
+}
+
+writeCards('name', 'event');
+
+function countDown(init = 10) {
+    while (init >= 0) {
+        console.log(init--);
+    }
+}
